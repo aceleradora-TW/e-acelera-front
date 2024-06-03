4f1487d26a4568671bc695ea2dc5e52ae2e0d929
Wanderson Silva <wandersonsilva@Mac-mini-de-Wanderson.local>
2024-06-03 10:32:51 -0300

    Feat(src/componentes/NavBar.tsx): criando componente navbar

---
+++ src/componentes/NavBar.tsx
@@ -0,0 +1,15 @@
+import { AppBar, Paper, Toolbar } from "@mui/material"
+
+export default function NavBar () {
+    return (
+        <>
+        <AppBar>
+        <Paper>
+            <Toolbar>
+                
+            </Toolbar>
+        </Paper>
+        </AppBar>
+        </>
+    )
+}
