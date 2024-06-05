6ecb01b8b91a4c9a1f8eb285721d852131ebd3e3
Wanderson Silva <wandersonsilva@Mac-mini-de-Wanderson.local>
2024-06-05 14:55:50 -0300

    POC(src/app/page.tsx): requisitando api e mapeando dados - @wander06 @emilycavalcanti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 'use client';
 import Image from "next/image"
-import react from "../../public/assets/react.png"
-import js from "../../public/assets/js.png"
-import next from "../../public/assets/next.png"
 import { AppBar, Box, Button, Card, CardActionArea, CardContent, CssBaseline, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeConfig } from '@/componentes/config/theme';
+import { useEffect, useState } from "react";
 
 const drawerWidth = 0;
 
 export default function Home() {
+  const [data, setData] = useState([])
+  
+  useEffect (()=>{
+    async function listagemTemas() {
+      try {
+        const apiKey = process.env.NEXT_PUBLIC_SECRECT_KEY || ""
+        console.log(apiKey)
+        const data = await fetch("https://stackby.com/api/betav1/rowlist/stqB2IjOCulBJkhrZB/Nivelamento", {
+          method:"GET", 
+          headers: {
+            "x-api-key": apiKey,
+            "Content-Type": "application/json"
+          }
+        })
+        
+        const imprimeData = await data.json()
+        console.log(imprimeData)
+        setData(imprimeData.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    listagemTemas()
+  } ,[])
+
   return (
     <main>
       <ThemeConfig>
@@ -32,7 +55,6 @@ export default function Home() {
               >
                 <MenuIcon />
               </IconButton>
-
               <Typography variant="h6" noWrap component="div" flexGrow={1} sx={{ cursor: "pointer" }}>
                 Home
               </Typography>
@@ -60,69 +82,38 @@ export default function Home() {
             sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
           >
             <Toolbar />
-            <CardContent>
-              <Typography variant="h5" component="div" color="black">
-                React
-              </Typography>
-              <Typography variant="body2" color="black">
-                11/02/2024
-              </Typography>
+            <div>
+              {/* {console.log(data)} */}
+              {data.map((element: any, index: any) => ( 
+                <div key={index}>
+                  <CardContent>
+                    <Typography variant="h5" component="div" color="black">
+                     {element.field.Name}
+                    </Typography>
+                    <Typography variant="body2" color="black">
+                      {element.field.createdAt}
+                    </Typography>
 
-              <Typography variant="body2" color="black">
-                Resume: Aula avançada de React.
-              </Typography>
-            </CardContent>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <Image
-                  width={300}
-                  src={react}
-                  alt="imagem filme"
-                />
-              </CardActionArea>
-            </Card>
-            <CardContent>
-              <Typography variant="h5" component="div" color="black">
-                JavaScript
-              </Typography>
-              <Typography variant="body2" color="black">
-                01/02/2024
-              </Typography>
-
-              <Typography variant="body2" color="black">
-                Resume: Aula avançada de JavaScript.
-              </Typography>
-            </CardContent>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <Image
-                  width={300}
-                  src={js}
-                  alt="imagem filme"
-                />
-              </CardActionArea>
-            </Card>
-            <CardContent>
-              <Typography variant="h5" component="div" color="black">
-                Next.js
-              </Typography>
-              <Typography variant="body2" color="black">
-                13/02/2024
-              </Typography>
-
-              <Typography variant="body2" color="black">
-                Resume: Aula avançada de next.js.
-              </Typography>
-            </CardContent>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <Image
-                  width={300}
-                  src={next}
-                  alt="imagem filme"
-                />
-              </CardActionArea>
-            </Card>
+                    <Typography variant="body2" color="black">
+                      {element.field.Descrição}
+                    </Typography>
+                  </CardContent><Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                      {element.field.Imagem.map((element: any, index: any) => (
+                        <Image
+                        key={index}
+                        src={element.url}
+                        width={300}
+                        height={300}
+                        alt="imagem filme" />
+                      ))}
+                      
+                    </CardActionArea>
+                  </Card>
+                </div>
+              ))}
+            </div>
+            
           </Box>
         </Box>
       </ThemeConfig>
