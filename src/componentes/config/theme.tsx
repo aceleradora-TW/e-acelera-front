b7d4494c27d7b8467214a32a871c8d47a71b38fc
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-06-11 10:51:33 -0300

    Feat(src/componentes/config/theme.tsx) adicao de novas cores e alteracao de hex para rgb @stephanyfps @ledatrindade

--- src/componentes/config/theme.tsx
+++ src/componentes/config/theme.tsx
@@ -1,75 +1,54 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
-import React from "react"
-
-// const theme = createTheme({
-//     palette: {
-//       primary: {
-//         // light: será calculada com base em palette.primary.main,
-//         main: '#ff4400',
-//         // dark: será calculada com base em palette.primary.main,
-//         // contrastText: será calculada para contrastar com palette.primary.main
-//       },
-//       secondary: {
-//         light: '#0066ff',
-//         main: '#0044ff',
-//         // dark: será calculada com base palette.secondary.main,
-//         contrastText: '#ffcc00',
-//       },
-//       // Usado por `getContrastText()` para maximizar o contraste entre
-//       // o plano de fundo e o texto.
-//       contrastThreshold: 3,
-//       // Usado pelas funções abaixo para mudança de uma cor de luminância por aproximadamente
-//       // dois índices dentro de sua paleta tonal.
-//       // Por exemplo, mude de Red 500 para Red 300 ou Red 700.
-//       tonalOffset: 0.2,
-//     },
-//   });
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import React from "react";
 
 type ThemeProp = {
-    children: JSX.Element
-}
+  children: JSX.Element;
+};
 
 export enum themePalette {
-    buttonText = "#031D3C",
-    title = " #002C53",
-    buttonBg = "#5FA0ED",
-    descriptionCard = "#02C2C2C",
-    descriptionText = "#000000",
-    statusInProgress= "#FFDE6B",
-    statusConcluded = "#9EFF85",
-    statusPending = "#DFDFDF"
+  buttonText = "RGB(3, 29, 60)",
+  title = "RGB(0, 44, 83)",
+  buttonBg = "RGB(95, 160, 237)",
+  descriptionCard = "RGB(44, 44, 44)",
+  descriptionText = "RGB(0, 0, 0)",
+  statusInProgress = "RGB(225, 222, 107)",
+  statusConcluded = "RGB(3, 29, 60)",
+  statusPending = "RGB(225, 225, 225)",
+  mainBg = "RGB(245, 245, 245)",
+  cardBg = "RGB(255, 255, 255)",
+  shawdowBg = "RGBA(0, 0, 0, 0.25)",
 }
 
 const theme = createTheme({
-    palette: {
-        // mode: "dark",
-        primary: {
-            main: themePalette.title,
-            light: themePalette.buttonBg, 
-            dark: themePalette.descriptionText ,
-            contrastText: themePalette.descriptionCard
-        },
-        
-        secondary: {
-            main: themePalette.buttonText,
-            light: themePalette.statusPending, 
-            dark: themePalette.statusInProgress,
-            contrastText: themePalette.statusConcluded
-        },
-
-        
+  palette: {
+    primary: {
+      main: themePalette.title,
+      light: themePalette.buttonBg,
+      dark: themePalette.descriptionText,
+      contrastText: themePalette.descriptionCard,
     },
 
-   
-})
+    secondary: {
+      main: themePalette.buttonText,
+      light: themePalette.statusPending,
+      dark: themePalette.statusInProgress,
+      contrastText: themePalette.statusConcluded,
+    },
+    info: {
+      main: themePalette.mainBg,
+      light: themePalette.cardBg,
+      dark: themePalette.shawdowBg,
+    },
+  },
+});
 
 export const ThemeConfig: React.FC<ThemeProp> = ({ children }) => {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {children}
-        </ThemeProvider>
-    )
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
 
-export { theme }; 
+export { theme };
