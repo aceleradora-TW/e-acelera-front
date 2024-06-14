5220091938a7d0d2641912304cb6b69b9eddf5df
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-06-14 14:02:19 -0300

    refactor(tailwind.config.ts) remocao das cores no tailwind @ledatrindade @stephanybrazeir0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/componentes/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -12,20 +12,6 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-          color: {
-            "buttonText": "#031D3C",
-            "title": " #002C53",
-            "buttonBg": "#5FA0ED",
-            "descriptionCard": "#02C2C2C",
-            "descriptionText": "#000000",
-            "statusInProgress": "#FFDE6B",
-            "statusConcluded": "#9EFF85",
-            "statusPending": "#DFDFDF", 
-            "mainBg": "#F5F5F5",
-            "cardBg": "#FFFFFF",
-          },
       },
     },
   },
