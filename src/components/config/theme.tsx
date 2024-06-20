2d5f70305b806ea9f094242c36ed7cebcf305558
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-06-20 10:12:50 -0300

    feat(src/components/config/theme.tsx) alteracoes nas cores

--- src/components/config/theme.tsx
+++ src/components/config/theme.tsx
@@ -6,16 +6,15 @@ type ThemeProp = {
 };
 
 export enum themePalette {
-  buttonText = "rgb(3, 29, 60)",
+  button = "rgb(11, 73, 122)",
   title = "rgb(0, 44, 83)",
-  buttonBg = "rgb(122, 175, 240)",
   descriptionCard = "rgb(44, 44, 44)",
   descriptionText = "rgb(0, 0, 0)",
   statusInProgress = "rgb(225, 222, 107)",
-  statusConcluded = "rgb(3, 29, 60)",
+  statusConcluded = "rgb(155, 255, 133)",
   statusPending = "rgb(225, 225, 225)",
   mainBg = "rgb(245, 245, 245)",
-  cardBg = "rgb(255, 255, 255)",
+  whiteBg = "rgb(255, 255, 255)",
   shawdowBg = "rgba(0, 0, 0, 0.25)",
 }
 
@@ -23,22 +22,11 @@ const theme = createTheme({
   palette: {
     primary: {
       main: themePalette.title,
-      light: themePalette.buttonBg,
+      light: themePalette.button,
       dark: themePalette.descriptionText,
       contrastText: themePalette.descriptionCard,
     },
 
-    secondary: {
-      main: themePalette.buttonText,
-      light: themePalette.statusPending,
-      dark: themePalette.statusInProgress,
-      contrastText: themePalette.statusConcluded,
-    },
-    info: {
-      main: themePalette.mainBg,
-      light: themePalette.cardBg,
-      dark: themePalette.shawdowBg,
-    },
   },
   typography: {
     h1: {
