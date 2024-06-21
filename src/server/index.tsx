fe0e26b108975d4e03f10647a91fa2b80f19d0b8
Wanderson Silva <wandersonsilva@Mac-mini-de-Wanderson.local>
2024-06-21 14:03:53 -0300

    POC(src/server/index.tsx): listando temas - @laerciomelo @wander06

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,33 +1,53 @@
-
-
-function getData() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Baaaaaaaaah')
-        }, 2000)
-    })
-}
+// function getData() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Baaaaaaaaah')
+//         }, 2000)
+//     })
+// }
+import { AppBar, Box, Button, Card, CardActionArea, CardContent, CssBaseline, IconButton, Toolbar, Typography } from "@mui/material";
 
 export default async function ListServerSide() {
     const fetchData = async () => {
         try {
-            const apiKey = await fetch("https://stackby.com/api/betav1/rowlist/stqB2IjOCulBJkhrZB/Themes", {
+            const apiKey = process.env.STACKBY_SECRET_KEY || ""
+            console.log(apiKey)
+            const fetchData = await fetch("https://stackby.com/api/betav1/rowlist/stqB2IjOCulBJkhrZB/Themes", {
                 method: "GET",
                 headers: {
-                    "x-api-key": "jOELi8SJTNSw0qVy",
+                    "x-api-key": apiKey,
                     "Content-Type": "application/json"
                 }
             })
-            return apiKey.json()
+            const data = fetchData.json()
+            return data
         } catch(error) {
             console.log(error)
         }
     } 
-    const nivelamento = await fetchData()
-    console.log(nivelamento)
+    const renderData = await fetchData()
+    console.log(renderData)
     return (
         <>
+            <div>
+            {renderData.data.map((element: any, index: any) => ( 
+                <div key={index}>
+                  <CardContent>
+                    <Typography variant="h5" component="div" color="black">
+                     {element.field.Title}
+                    </Typography>
+                    <Typography variant="body2" color="black">
+                      {element.field.Description}
+                    </Typography>
 
+                    <Typography variant="body2" color="black">
+                      {element.field.createdAt}
+                    </Typography>
+                  </CardContent>
+                </div>
+              ))}
+            </div>
         </>
     )
+    
 }
