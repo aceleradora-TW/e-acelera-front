ce636c0a62031ce98926e22a5f7a63c1b1519f91
LedaTrindade <ledatrindade94@gmail.com>
2024-06-24 10:54:12 -0300

    Feat (src/components/Cards/index.tsx -> src/components/BaseCard/index.tsx | src/components/ButtonCard/index.tsx): ajustando a tipografia e mudanças solicitadas pelo QA - @ledatrindade @VivianVidal

--- src/components/BaseCard/index.tsx
+++ src/components/BaseCard/index.tsx
@@ -6,15 +6,17 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea, CardActions } from '@mui/material';
 import { ClickButton } from '../ClickButton';
 import { useRouter } from 'next/navigation';
+import { blue, red } from '@mui/material/colors';
 
 interface CardProps{
     title: string;
     description: string;
+    textImage?: string; 
     image?: string;
     route: string;
 }
 
-export const BaseCard: React.FC<CardProps> = ({title, description, image, route}) => {
+export const BaseCard: React.FC<CardProps> = ({title, description, textImage, image, route}) => {
     const router = useRouter()
     const handlerClicker = (route: string) => {
         router.push(`/${route}`)
@@ -27,24 +29,39 @@ export const BaseCard: React.FC<CardProps> = ({title, description, image, route}
                 <CardMedia
                     component="img"
                     image={image}
-                    alt="green iguana"
+                    alt={textImage}
                     sx={{ height: 152, marginBottom: 5.5}}
                 />
             )}
-                <CardContent sx={{ paddingBottom: 10.63 }}>
-                    <Typography gutterBottom variant="h3" component="div" sx={{
-                        maxHeight: 5,
-                        wordWrap: 'break-word'
+
+                {/* titulo e descrição */}
+                <CardContent sx={{ paddingBottom: 10.63, maxHeight: 250 }}> 
+                    <Typography gutterBottom variant="h2" component="div"  sx={{
+                        fontSize: "25.6px", 
+                        lineHeight: 1.6,
+                        fontWeight: 500,
+                        letterSpacing: 0.15,
+                        wordWrap: 'break-word',
+                        
                     }}>
                         {title}
                     </Typography>
-                    <Typography variant="body2">
+                    <Typography variant="body1"  sx={{
+                        wordWrap: 'break-word',
+                        display: '-webkit-box',
+                        WebkitLineClamp: 6,
+                        WebkitBoxOrient: 'vertical',
+                        overflowY: 'auto'
+                        
+                    }}>
                         {description}
                     </Typography>
                 </CardContent>
+
+                    {/* Botão */}
             </CardActionArea>
-            <CardActions sx={{ paddingBottom: 4}}>
-                <ClickButton title="Entrar" click={() => handlerClicker(route)} />
+            <CardActions sx={{ paddingBottom: 4, height: 20}}>
+                <ClickButton title="Entrar" click={() => handlerClicker(route)}  />
             </CardActions>
         </Card>
     );
--- src/components/ButtonCard/index.tsx
+++ src/components/ButtonCard/index.tsx
@@ -20,18 +20,31 @@ export const ButtonCard: React.FC<ButtonCardProps> = ({title, description, route
     }
     return (
 
-        <Card sx={{ maxWidth: 368, borderWidth: 2,
+        <Card sx={{ maxWidth: 316, borderWidth: 2, maxHeight: 256,
             "&:hover": {
                 transform: "scale(1.1)"
                            
             }
         }}>
             <CardActionArea onClick={() => handlerClicker(route)}>
-                <CardContent sx={{ paddingBottom: 5, paddingTop: 3}}>
-                    <Typography gutterBottom variant="h5" component="div">
+                <CardContent sx={{ paddingBottom: 1, paddingTop: 3}}>
+                <Typography gutterBottom variant="h2" component="div"  sx={{
+                        fontSize: "25.6px", 
+                        lineHeight: 1.6,
+                        fontWeight: 500,
+                        letterSpacing: 0.15,
+                        wordWrap: 'break-word',
+                        
+                    }}>
                         {title}
                     </Typography>
-                    <Typography variant="body2">
+                    <Typography variant="body1"  sx={{
+                        wordWrap: 'break-word',
+                        display: '-webkit-box',
+                        WebkitLineClamp: 4,
+                        WebkitBoxOrient: 'vertical',
+                        overflowY: 'auto'
+                    }}>
                         {description}
                     </Typography>
                 </CardContent>
