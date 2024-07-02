194f3e65150691f9e756f110dec1944f309f0114
emilycavalcanti <emilycalvacanti.s@gmail.com>
2024-07-02 10:51:16 -0300

    feat (src\app\page.tsx) modificação da tag selectStatus na page - @kesia-salgado @emilycavalcanti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,157 @@ import { ClickButton } from "@/components/ClickButton"
 import { useRouter } from "next/navigation"
 import { theme } from "@/components/config/theme";
 
-const drawerWidth = 0
+const drawerWidth = 0;
 
 export default function Home() {
-  const router=useRouter()
-  function clickTest(){
-    router.push("/")
+  const router = useRouter();
+
+  function clickTest() {
+    router.push("/");
   }
-  
+
+  return (
+    <ThemeConfig>
+      <Box className={"md:border-4"}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+            color: "white",
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              className={"text-color-statusInProgress"}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              flexGrow={1}
+              sx={theme.customStyles.button}
+            >
+              Home
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              flexGrow={1}
+              sx={{ cursor: "pointer" }}
+            >
+              Nivelamento
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              flexGrow={1}
+              sx={{ cursor: "pointer" }}
+            >
+              AutoEstudo
+            </Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              flexGrow={1}
+              sx={{ cursor: "pointer" }}
+            >
+              Feedback
+            </Typography>
+
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        ></Box>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            backgroundColor: "white",
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+          }}
+        >
+          <Toolbar />
+          <CardContent>
+            <Typography variant="h5" component="div" color="black">
+              React
+            </Typography>
+            <Typography variant="body2" color="black">
+              11/02/2024
+            </Typography>
+
+            <Typography variant="body2" color="black">
+              Resume: Aula avançada de React.
+            </Typography>
+          </CardContent>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <Image width={300} src={react} alt="imagem filme" />
+            </CardActionArea>
+          </Card>
+          <ClickButton title="Próximo exercício" click={clickTest} />
+          <ClickButton title="Entrar" click={clickTest} />
+          <StatusSelect />
+          <CardContent>
+            <Typography variant="h5" component="div" color="black">
+              JavaScript
+            </Typography>
+            <Typography variant="body2" color="black">
+              01/02/2024
+            </Typography>
+
+            <Typography variant="body2" color="black">
+              Resume: Aula avançada de JavaScript.
+            </Typography>
+          </CardContent>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <Image width={300} src={js} alt="imagem filme" />
+            </CardActionArea>
+          </Card>
+          <ClickButton title="Próximo exercício" click={clickTest} />
+          <ClickButton title="Entrar" click={clickTest} />
+          <StatusSelect />
+          <CardContent>
+            <Typography variant="h5" component="div" color="black">
+              Next.js
+            </Typography>
+            <Typography variant="body2" color="black">
+              13/02/2024
+            </Typography>
+
+            <Typography variant="body2" color="black">
+              Resume: Aula avançada de next.js.
+            </Typography>
+          </CardContent>
+
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <Image width={300} src={next} alt="imagem filme" />
+            </CardActionArea>
+          </Card>
+          <ClickButton title="Próximo exercício" click={clickTest} />
+          <ClickButton title="Entrar" click={clickTest} />
+          <StatusSelect width="30%"/>
+        </Box>
+      </Box>
+    </ThemeConfig>
+  );
 }
