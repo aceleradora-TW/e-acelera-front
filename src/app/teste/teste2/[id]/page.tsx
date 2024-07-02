f26e4a884104d61e1db7c1e54eb8eca394166c0b
thayanneregoo <thayanneregoo@hotmail.com>
2024-07-02 14:43:50 -0300

    FEAT (src\components\BreadCrumber\index.tsx) Cria BreadCrumb e adciona paginas de teste @thayanneregoo @stephanybrazeir0 @VivianVidal

---
+++ src/app/teste/teste2/[id]/page.tsx
@@ -0,0 +1,9 @@
+import BreadCrumb from "@/components/BreadCrumber";
+
+export default function Page (){
+    return(
+        <>
+        <BreadCrumb/>
+        </>
+    )
+}
---
+++ src/components/BreadCrumber/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+import * as React from "react";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "next/link";
+import Stack from "@mui/material/Stack";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { usePathname, useRouter } from "next/navigation";
+
+
+export default function BreadCrumb() {
+  const pathname = usePathname()
+  const breadcrumbs = pathname.split("/");
+  let newRoute = "";
+  const pathConcat = (path: string) => {
+    if (path !== "") {
+      newRoute += `/${path}`;
+    }
+  }
+  return (
+    <Stack spacing={2}>
+      <Breadcrumbs
+        separator={<NavigateNextIcon fontSize="small" />}
+        aria-label="breadcrumb"
+      >
+        {breadcrumbs.map((path: string) => (
+          <>
+            {pathConcat(path)}
+            <Link
+              color="inherit"
+              href={newRoute==''? '/':newRoute}
+            >
+              {path == "" ? "Home" : path}
+            </Link>
+          </>
+        ))}
+      </Breadcrumbs>
+    </Stack>
+  );
+}
