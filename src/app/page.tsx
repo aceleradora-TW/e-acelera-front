6698d5f9076b596674b6961dde0ffa842bb44294
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-07-12 15:03:01 -0300

    Feat(components/description/referencedescription/index.tsx) modifica a aparencia dos links  @fernandesmelo @emilycavalcanti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,12 @@ import next from "../../public/assets/next.png"
 import { Box, Card, CardActionArea, CardContent, CssBaseline, Toolbar, Typography } from "@mui/material"
 import { ThemeConfig } from '@/components/config/theme'
 import { theme } from '@/components/config/theme'
-import { ReferenceDescription } from "@/components/Description/ReferenceDescription"
 
 const drawerWidth = 0;
 
 export default function Home() {
   return (
     <main>
-      <ReferenceDescription links={["https://www.google.com.br/"]}/>
+      
     </main>
   )}
