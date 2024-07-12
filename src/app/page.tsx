bea90d607c2a06ccf8aef68db1868da6b99c6350
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-07-12 14:50:04 -0300

    Feat(app/pages.tsx) apaga componente de pages @fernandesmelo @emilycavalcanti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,6 @@ const drawerWidth = 0;
 export default function Home() {
   return (
     <main>
-      <ThemeDescription text="Olá, bom dia pessoal!! Espero que a mensagem lhes encontre com saúde. 
-      Aqui quem vos fala é Fernanda Lopes, diretora de programas do Baobá. Muitas, muitos, muites ainda não me conhecem. Estava em licença maternidade quando vocês se juntaram a nós.
-      Hoje escrevo para dar uma dica: o MOVER – MOVIMENTO PELA EQUIDADE RACIAL, apoiador do edital Carreiras em Movimento, lançou um espaço virtual com trilhas de aprendizagem e a oportunidade de se cadastrar no banco de talentos de 54 empresas associadas.
-      Não temos envolvimento algum mas soubemos que a plataforma, entre outras coisas, será um instrumento de match entre talentos (vocês e milhões de outros rsrs) e empresas. Pode ser uma boa chance de ampliação da empregabilidade e acesso a conhecimentos. 
-      Se tiverem um tempinho e interesse, façam o cadastro, explorem e aproveitem!!!!"/> 
+    
     </main>
   )}
