028dbe19eb64ab29f57f0dafa866f068eb9d80e1
thayanneregoo <thayanneregoo@hotmail.com>
2024-07-15 11:54:04 -0300

    FEAT (src\app\page.tsx) cria cards com mapeamento dados - @thayanneregoo @VivianVidal @stephanybrazeir0 @Lipe29bm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-
+'use client'
+import Image from "next/image"
+import react from "../../public/assets/react.png"
+import js from "../../public/assets/js.png"
+import next from "../../public/assets/next.png"
+import { Box, Card, CardActionArea, CardContent, CssBaseline, Toolbar, Typography } from "@mui/material"
+import { ThemeConfig } from '@/components/config/theme'
+import { theme } from '@/components/config/theme'
+import React from "react"
+import { ApiResponse } from "@/types/type"
 
 const drawerWidth = 0;
 
 export default async function Home() {
   return (
     <main>
+         {renderData &&  renderData.data.map((element: any, index: any) => ( 
+                <div key={index}>
+                  <CardContent>
+                    <Typography variant="h5" component="div" color="black">
+                     {element.field.Title}
+                    </Typography>
+                    <Typography variant="body2" color="black">
+                      {element.field.Description}
+                    </Typography>
 
+                    <Typography variant="body2" color="black">
+                      {element.field.createdAt}
+                    </Typography>
+                  </CardContent>
+                </div>
+              ))}
     </main>
   )}
