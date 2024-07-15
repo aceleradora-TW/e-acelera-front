857003d6c2408a66c43c7a0d247184ba9a661cbc
thayanneregoo <thayanneregoo@hotmail.com>
2024-07-15 11:54:04 -0300

    FEAT (src\app\page.tsx) cria cards com mapeamento dados - @thayanneregoo @VivianVidal @stephanybrazeir0 @Lipe29bm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 'use client'
-import { Box, CardContent, Grid, Typography } from "@mui/material"
+import Image from "next/image"
+import react from "../../public/assets/react.png"
+import js from "../../public/assets/js.png"
+import next from "../../public/assets/next.png"
+import { Box, Card, CardActionArea, CardContent, CssBaseline, Toolbar, Typography } from "@mui/material"
+import { ThemeConfig } from '@/components/config/theme'
+import { theme } from '@/components/config/theme'
 import React from "react"
 import { ApiResponse } from "@/types/type"
-import { BaseCard } from "@/components/BaseCard";
-import { Title } from "@/components/title";
+
+const drawerWidth = 0;
 
 export default function Home() {
   const [renderData, setRenderData] = React.useState<ApiResponse>();
@@ -31,7 +37,22 @@ export default function Home() {
   return (
     <>
     <main>
+         {renderData &&  renderData.data.map((element: any, index: any) => ( 
+                <div key={index}>
+                  <CardContent>
+                    <Typography variant="h5" component="div" color="black">
+                     {element.field.Title}
+                    </Typography>
+                    <Typography variant="body2" color="black">
+                      {element.field.Description}
+                    </Typography>
 
+                    <Typography variant="body2" color="black">
+                      {element.field.createdAt}
+                    </Typography>
+                  </CardContent>
+                </div>
+              ))}
     </main>
     </>
   )}
