28ec1677a7f5a55c78cf8480c01ead1f6f187902
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2024-07-17 15:01:56 -0300

    Refactor(src/app/page.tsx): Resolvendo conflito da branch -@angeloneves19 -@VivianVidal -@Lip29bm -@wander06

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,44 +6,48 @@ import { BaseCard } from "@/components/BaseCard";
 import { Title } from "@/components/title";
 
 export default function Home() {
+
   const [renderData, setRenderData] = React.useState<ApiResponse>();
-  const pagina = 'Nivelamento'
+  const pagina = "Nivelamento";
 
   React.useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await fetch(`/api/stackbyApi/Themes`, {
-                method: "GET",
-            });
-            const parseData: ApiResponse = await response.json();
-            setRenderData(parseData);
-            console.log(await parseData)
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
+      try {
+        const response = await fetch(`/api/stackbyApi/Themes`, {
+          method: "GET",
+        });
+        const parseData: ApiResponse = await response.json();
+        setRenderData(parseData);
+        console.log(await parseData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
     fetchData();
-}, []);
+  }, []);
 
-  console.log(renderData)
+  console.log(renderData);
+
+
 
   return (
     <>
-    <main>
-      <Grid container  spacing={2} sx={{margin:'0 80px'}}>
-      <Grid item xs={12}> <Title text={`Bem vindo ao ${pagina}`}/> </Grid>
-         {renderData &&  renderData.data.filter((element:any)=>element.field.category === pagina).map((element: any, index: any) => ( 
-                <Grid item xs={4} key={index}>                  
-                  <BaseCard 
-                  title={element.field.title} 
-                  description={element.field.description} 
-                  route={element.id} 
-                  image={element.field.image ? element.field.image[0].url: ''}
-                  />
-                </Grid>
-              ))}
+      <Grid container mx={10} columns={{ xl: 12, lg: 9, md: 6, sm: 3 }} rowSpacing={3}>
+        <Grid item xl={12} lg={9} md={6} sm={3}>
+          <Title text={`Bem vindo ao ${pagina}`} />
+        </Grid>
+        {renderData && renderData.data.filter((element: any) => element.field.category === pagina).map((element: any, index: any) => (
+              <Grid item xl={4} lg={3} md={3} sm={3} justifyContent="center" key={index}>
+                <BaseCard
+                  title={element.field.title}
+                  description={element.field.description}
+                  route={element.id}
+                  image={element.field.image ? element.field.image[0].url : ""}
+                />
+              </Grid>
+            ))}
       </Grid>
-    </main>
     </>
-  )}
+  )
+}
