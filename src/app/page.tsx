98f8c5a21bc4a343bd547260f1b65db0b9197fbd
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-07-19 13:32:53 -0300

    atualiza pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,16 @@ import react from "../../public/assets/react.png"
 import js from "../../public/assets/js.png"
 import next from "../../public/assets/next.png"
 import { Box, Card, CardActionArea, CardContent, CssBaseline, Toolbar, Typography } from "@mui/material"
-import { ThemeConfig } from '@/app/config/theme'
-import { theme } from '@/app/config/theme'
-import { ThemeDescription } from "@/components/Description/ThemeDescription"
+import { ThemeConfig } from '@/components/config/theme'
+import { theme } from '@/components/config/theme'
 
 const drawerWidth = 0;
 
 export default function Home() {
+
+ 
   return (
     <main>
-      <ThemeDescription text="Um texto é uma manifestação da linguagem. Pode ser definido como tudo aquilo que é dito por um emissor e interpretado por um receptor. Dessa forma, tudo que é interpretável é um texto. Outra forma de conceituação é pensar que tudo aquilo que produz um sentido completo, que seja uma mensagem compreensível, é um texto. Um texto é uma manifestação da linguagem. Pode ser definido como tudo aquilo que é dito por um emissor e interpretado por um receptor. Dessa forma, tudo que é interpretável é um texto. Outra forma de conceituação é pensar que tudo aquilo que produz um sentido completo, que seja uma mensagem compreensível, é um texto."/>
+
     </main>
   )}
