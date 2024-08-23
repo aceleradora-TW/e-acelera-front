35e50cea3e4bfe53f94a45ebb3cd5d7a2894192d
Felipe <felipe-bmachado@educar.rs.gov.br>
2024-08-23 11:37:35 -0300

    feat (src/components/CardVideo/index.tsx) adicionamos suporte a markdown @emilycavalcanti @stephanybrazeir0 @Lip29bm @wander06

--- src/components/CardVideo/index.tsx
+++ src/components/CardVideo/index.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { ThemeConfig, theme } from "../../app/config/theme";
 import StatusSelect from "@/components/StatusSelect";
+import ReactMarkdown from "react-markdown";
 
-
+const components = {
+  h1: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Typography variant="h2" {...props} />
+  ),
+  p: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <Typography variant="body1" sx={{ color: theme.palette.textColor?.light }} {...props} />
+  ),
+};
 function CardVideo() {
   const videoUrl: string = "https://www.youtube.com/watch?v=KvcneqbMJs4";
-  let text: string = "Neste curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente! Neste curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente!";
-  let titulov: string = "Curso de TypeScript para Completos Iniciantes";
+  let text: string = "**Neste** curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente! Neste curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente!";
+  let titulov: string = "# Curso de TypeScript para Completos Iniciantes";
   let creditos: string = "Felipe Rocha • dicasparadevs";
 
   function getYouTubeEmbedUrl(url: string): string {
@@ -16,35 +24,31 @@ function CardVideo() {
   }
 
   return (
-      <Box sx={ theme.customStyles.cardVideo }>
-        <Box sx={ theme.customStyles.cardVideoLink }>
-          <iframe
-            width="100%"
-            height="332"
-            src={getYouTubeEmbedUrl(videoUrl)}
-          ></iframe>
-          
-
-          <Box sx={theme.customStyles.cardVideoNumber}>
-            <Typography variant="h2">
-              {titulov}
-            </Typography>
-            <Typography variant="body1" sx={{color:theme.palette.textColor?.light}}>
-              {creditos}
-            </Typography>
-          </Box>
-
-          <Box sx={ theme.customStyles.cardVideoDescription}>
-            <Typography variant="body1"  sx={{color:theme.palette.textColor?.light}}>
-              {text}
-            </Typography>
-          </Box>
-
-          <Box sx={theme.customStyles.cardVideoSelect}>
-            <StatusSelect />
-          </Box>
+    <Box sx={theme.customStyles.cardVideo}>
+      <Box sx={theme.customStyles.cardVideoLink}>
+        <iframe
+          width="100%"
+          height="332"
+          src={getYouTubeEmbedUrl(videoUrl)}
+        ></iframe>
+        <Box sx={theme.customStyles.cardVideoNumber}>
+          <ReactMarkdown components={components}>
+            {titulov}
+          </ReactMarkdown>
+          <ReactMarkdown components={components}>
+            {creditos}
+          </ReactMarkdown>
+        </Box>
+        <Box sx={theme.customStyles.cardVideoDescription}>
+          <ReactMarkdown components={components}>
+            {text}
+          </ReactMarkdown>
+        </Box>
+        <Box sx={theme.customStyles.cardVideoSelect}>
+          <StatusSelect />
         </Box>
       </Box>
+    </Box>
   );
 }
 
