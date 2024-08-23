99094db960ff959efb4b8be1dfc78bd7ef06e88f
Felipe <felipe-bmachado@educar.rs.gov.br>
2024-08-23 12:00:04 -0300

    feat ( src/components/cardVideo/index.tsx) refatoramos o codigo @stephanybrazeir0 @emilycavalcanti @wander06 @lip29bm

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -5,13 +5,13 @@ import { Grid } from "@mui/material"
 export default function Teste() {
     return(
         <Grid container >
-            
-           <Grid item xl={6} lg={6} md={6} sm={12}>
-           <CardVideo/>
-           </Grid>
 
            <Grid item xl={6} lg={6} md={6} sm={12}>
-           <CardVideo/>
+           <CardVideo title="# Curso de TypeScript para Completos Iniciantes" text="Neste curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente! Neste curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente!" linkVideo="https://www.youtube.com/watch?v=KvcneqbMJs4" references="Felipe Rocha • dicasparadevs"/>
+           </Grid> 
+
+           <Grid item xl={6} lg={6} md={6} sm={12}>
+           <CardVideo title="# Curso de TypeScript para Completos Iniciantes" text="**Neste** curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente! Neste curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente!" linkVideo="https://www.youtube.com/watch?v=KvcneqbMJs4" references="Felipe Rocha • dicasparadevs"/>
            </Grid>
         
         </Grid>
--- src/components/CardVideo/index.tsx
+++ src/components/CardVideo/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import { ThemeConfig, theme } from "../../app/config/theme";
+import { theme } from "../../app/config/theme";
 import StatusSelect from "@/components/StatusSelect";
 import ReactMarkdown from "react-markdown";
 
@@ -12,11 +12,13 @@ const components = {
     <Typography variant="body1" sx={{ color: theme.palette.textColor?.light }} {...props} />
   ),
 };
-function CardVideo() {
-  const videoUrl: string = "https://www.youtube.com/watch?v=KvcneqbMJs4";
-  let text: string = "**Neste** curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente! Neste curso ensinarei para você TUDO o que você precisa saber para adquirir uma base sólida no TypeScript, uma das linguagens mais requisitadas no mercado atualmente!";
-  let titulov: string = "# Curso de TypeScript para Completos Iniciantes";
-  let creditos: string = "Felipe Rocha • dicasparadevs";
+interface CardVideoProps{
+  text : string;
+  title: string;
+  linkVideo: string;
+  references?: string;
+}
+export const CardVideo: React.FC<CardVideoProps>=({ text, title, linkVideo, references }) => {
 
   function getYouTubeEmbedUrl(url: string): string {
     const videoId = url.split('v=')[1]?.split('&')[0];
@@ -29,14 +31,14 @@ function CardVideo() {
         <iframe
           width="100%"
           height="332"
-          src={getYouTubeEmbedUrl(videoUrl)}
+          src={getYouTubeEmbedUrl(linkVideo)}
         ></iframe>
         <Box sx={theme.customStyles.cardVideoNumber}>
           <ReactMarkdown components={components}>
-            {titulov}
+            {title}
           </ReactMarkdown>
           <ReactMarkdown components={components}>
-            {creditos}
+            {references}
           </ReactMarkdown>
         </Box>
         <Box sx={theme.customStyles.cardVideoDescription}>
