f73f4860125378dba6e3c4d79081f4043a5d39a5
wander06 <wandersonpsilva7@gmail.com>
2024-09-06 15:24:15 -0300

    feat(src/components/PageElements/Content): Movendo componentes de content para fora dos detailingContent - @Wander06 @Lip29bm

--- src/components/PageElements/Content/DetailingThemeContent/index.tsx
+++ src/components/PageElements/Content/DetailingThemeContent/index.tsx
@@ -10,27 +10,26 @@ interface DetailingContentProps {
   data: ApiResponse;
   id: string;
 }
-
+const ThemeContent: React.FC<{ field: ThemeField }> = ({ field }) => (
+  <>
+    <Grid item xl={12} lg={9} md={6} sm={3}>
+      <BreadCrumb />
+      <Title text={field.title} />
+    </Grid>
+    <DescriptionDivider text={field.description} />
+    <Grid item xl={12} lg={9} md={6} sm={3}>
+      <Title text={"Tópicos"} />
+    </Grid>
+    <ContainerCardTopics
+      topics={field.topics}
+      topicsDescription={field.topicsDescription}
+      topicsInfo={field.topicsInfo}
+    />
+  </>
+)
 export const DetailingThemeContent: React.FC<DetailingContentProps> = ({ data, id }) => {
   const filteredData = data?.data.filter((element: DataItem) => element.id === id.split("-")[0]);
-  const ThemeContent: React.FC<{ field: ThemeField }> = ({ field }) => (
-    <>
-      <Grid item xl={12} lg={9} md={6} sm={3}>
-        <BreadCrumb />
-        <Title text={field.title} />
-      </Grid>
-      <DescriptionDivider text={field.description} />
-      <Grid item xl={12} lg={9} md={6} sm={3}>
-        <Title text={"Tópicos"} />
-      </Grid>
-      <ContainerCardTopics
-        topics={field.topics}
-        topicsDescription={field.topicsDescription}
-        topicsInfo={field.topicsInfo}
-      />
-    </>
-  )
-
+  
   return (
     <>
       {filteredData
