a503fa2f6c24c2936ba38b2478f31e92bcd512d2
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-09-11 16:00:55 -0300

    fix(src/components/BadRequest/index.tsx): arrumando responsivo da page bad request @Wander06 @stephanybrazeir0

--- src/components/BadRequest/index.tsx
+++ src/components/BadRequest/index.tsx
@@ -1,13 +1,35 @@
-import { Box, Link, Typography } from "@mui/material"
-import svgBadRequest from "../../../public/assets/503-bad-request.svg"
-import Image from "next/image"
+import { Link, Typography, Grid} from "@mui/material";
+import svgBadRequest from "../../../public/assets/503-bad-request.svg";
+import { LayoutPage } from "../PageElements/LayoutPage";
+import Image from "next/image";
 
 export const BadRequest = () => {
-    return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "calc(100vh - 80px)", boxSizing: "border-box" }}>
-            <Image src={svgBadRequest} alt="Ilustração de um gato brincando com um novelo de lã, com os números 503 indicando erro de serviço indisponível."/>
-            <Typography variant="caption">Estamos passando por dificuldades para carregar o conteúdo. Por favor, tente novamente em instantes.</Typography>
-            <Link target="_blank" rel="noferrer" href="https://storyset.com/internet">Internet illustrations by Storyset</Link>
-        </Box>
-    )
-}
+  return (
+    <LayoutPage>
+      <Grid container direction="column" sx={{justifyContent: "center", alignItems: "center", width: "100%"}}>
+        <Grid item >
+          <Image
+            src={svgBadRequest}
+            alt="Ilustração de um gato brincando com um novelo de lã, com os números 503 indicando erro de serviço indisponível."
+            style={{display: "block", width: "100%", height: "auto"}}
+          />
+        </Grid>
+        <Grid item >
+          <Typography variant="caption">
+            Estamos passando por dificuldades para carregar o conteúdo. Por
+            favor, tente novamente em instantes.
+          </Typography>
+        </Grid>
+        <Grid item >
+          <Link
+            target="_blank"
+            rel="noferrer"
+            href="https://storyset.com/internet"
+          >
+            Internet illustrations by Storyset
+          </Link>
+        </Grid>
+      </Grid>
+    </LayoutPage>
+  );
+};
