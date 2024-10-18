bfd26185a1ae5a670296bc53391f73eb2ca4d48b
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-10-18 10:41:02 -0300

    fix(src/app/api/stackbyApi/[endpoint]/route.tsx)correcao da rota @stephanybrazeir0 @EmilyCavalcanti

--- src/app/api/stackbyApi/[endpoint]/route.tsx
+++ src/app/api/stackbyApi/[endpoint]/route.tsx
@@ -1,4 +1,4 @@
-import { ApiResponse, ThemeField } from "@/types/type";
+import { ApiResponse } from "@/types/type";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: { endpoint: string } }) {
@@ -25,33 +25,8 @@ export async function GET(req: Request, { params }: { params: { endpoint: string
         }
 
         const data: ApiResponse = await response.json();
-
-        let filteredData;
-
-        // // Verifique se o endpoint é "Themes" antes de filtrar
-        // if (endpoint === "Themes") {
-        //     filteredData = data.data.map((item: any) => {
-        //         const title = item.field.title;
-        //         const id = item.id;
-
-        //         // Adicione a lógica para obter a descrição e a imagem se disponíveis
-        //         if (item.field && (item.field as ThemeField).cardDescription && (item.field as ThemeField).image) { 
-        //             const themeField = item.field as ThemeField;
-        //             const cardDescription = themeField.cardDescription;
-        //             const image = themeField.image;
-
-        //             return { title, cardDescription, image, id };
-        //         }
-
-        //         // Caso o item não tenha cardDescription e image
-        //         return { title, id };
-        //     });
-        // } else {
-        //     // Caso não seja o endpoint "Themes", retorne os dados originais
-        //     filteredData = data.data; // Use os dados como estão
-        // }
-
-        return NextResponse.json(filteredData, {
+        
+        return NextResponse.json(data, {
             status: 200
         });
     } catch (error) {
@@ -62,56 +37,3 @@ export async function GET(req: Request, { params }: { params: { endpoint: string
         });
     }
 }
-
-
-// import useFetchData from "@/components/fetchData";
-// import { useState } from "react";
-// import { Loading } from "@/components/Loading";
-// import { PageThemesContent } from "../../Content/PageThemesContent";
-// import { LayoutPage } from "../../LayoutPage";
-// import { BadRequest } from "@/components/BadRequest";
-// import { NoData } from "@/components/NoData";
-
-// export const RenderThemePage = (category: string) => {
-//     const { data: renderData, isLoading: loading, error: error } = useFetchData('/api/stackbyApi/Themes');
-//     const [topicsData, setTopicsData] = useState(null);
-//     const [loadingTopics, setLoadingTopics] = useState(false);
-//     const [topicsError, setTopicsError] = useState(false);
-
-//     // Função para carregar os tópicos quando necessário
-//     const fetchTopics = async (themeId: string) => {
-//         setLoadingTopics(true);
-//         try {
-//             const response = await fetch(`/api/stackbyApi/Themes/${themeId}/Topics`);
-//             const data = await response.json();
-//             setTopicsData(data);
-//             setLoadingTopics(false);
-//         } catch (error) {
-//             setTopicsError(true);
-//             setLoadingTopics(false);
-//         }
-//     };
-
-//     if (loading) {
-//         return <Loading />
-//     }
-//     if (error) {
-//         return <BadRequest />
-//     }
-//     if (!renderData) {
-//         return <NoData />
-//     }
-
-//     return (
-//         <LayoutPage>
-//             <PageThemesContent 
-//                 data={renderData} 
-//                 category={category} 
-//                 onLoadTopics={fetchTopics}  // Adiciona função para carregar tópicos
-//                 topicsData={topicsData}     // Passa os tópicos carregados
-//                 loadingTopics={loadingTopics} // Estado de carregamento dos tópicos
-//                 topicsError={topicsError}   // Estado de erro nos tópicos
-//             />
-//         </LayoutPage>
-//     );
-// };
--- src/components/PageElements/Renders/RenderThemePage/index.tsx
+++ src/components/PageElements/Renders/RenderThemePage/index.tsx
@@ -4,41 +4,61 @@ import { PageThemesContent } from "../../Content/PageThemesContent";
 import { LayoutPage } from "../../LayoutPage";
 import { BadRequest } from "@/components/BadRequest";
 import { NoData } from "@/components/NoData";
-import { ThemeTeste, ApiResponse } from "@/types/type";
+import { ThemeTeste, ApiResponse, DataItem } from "@/types/type";
 
 export const RenderThemePage = (category: string) => {
-    const { data: renderData, isLoading: loading, error: error } = useFetchData(`/api/stackbyApi/Themes?nocache=${Date.now()}&field=ThemeTeste`);
+  const {
+    data: renderData,
+    isLoading: loading,
+    error: error,
+  } = useFetchData("/api/stackbyApi/Themes"); //?nocache=${Date.now()}&field=ThemeTeste
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <BadRequest />;
+  }
+  if (!renderData) {
+    return <NoData />;
+  }
 
-    if (loading) {
-        return <Loading />
-    }
-    if (error) {
-        return <BadRequest />
-    }
-    if (!renderData) {
-        return <NoData />
-    }
+  // const filteredData = renderData.data.filter((item: DataItem) => {
+  //   const title = item.field.title;
+  //   return title;
+  // });
 
-    // const filteredData = renderData.data.map((item) => {
-    //     const title = item.field.title;
-    //     const id = item.id;
+  // filteredData.forEach((item: DataItem) => {
+  //   console.log(item.field.title); // Mostra os títulos no console
+  // });
 
-    //     // Verifica se item.field é do tipo ThemeField, garantindo que tem cardDescription e image
-    //     if (item.field && (item.field as ThemeField).cardDescription && (item.field as ThemeField).image) { 
-    //         const themeField = item.field as ThemeField;
-    //         const cardDescription = themeField.cardDescription;
-    //         const image = themeField.image;
+  const filteredData = renderData.data
+    .filter((item: DataItem) => {
+      return (
+        item.field.title &&
+        "cardDescription" in item.field &&
+        "image" in item.field
+      );
+    })
+    .map((item: DataItem) => {
+      const theme = item.field as ThemeTeste;
 
-    //         return { title, cardDescription, image, id };
-    //     }
+      return {
+        title: theme.title,
+        cardDescription: theme.cardDescription,
+        image: theme.image,
+        category: theme.category,
+      };
+    });
 
-    //     // Caso o item não tenha cardDescription e image
-    //     return { title, id };
-    // });
+  console.log(filteredData);
 
-    return (
-        <LayoutPage>
-            <PageThemesContent data={renderData} category={category} />
-        </LayoutPage>
-    );
+  if (filteredData.length === 0) {
+    return <NoData />;
+  }
+
+  return (
+    <LayoutPage>
+      <PageThemesContent data={renderData} category={category} />
+    </LayoutPage>
+  );
 };
