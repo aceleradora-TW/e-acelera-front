797e9bdbf1c8f822786888c04ff033c322998f3f
mylenaverspeelt <mylena49@hotmail.com>
2024-10-23 09:27:24 -0300

    merge da main com a staging @mylenaverspeelt

--- src/app/api/stackbyApi/[endpoint]/route.tsx
+++ src/app/api/stackbyApi/[endpoint]/route.tsx
@@ -1,12 +1,23 @@
 import { ApiResponse } from "@/types/type";
+import { error } from "console";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: { endpoint: string } }) {
     try {
         const apiKey: string = process.env.STACKBY_SECRET_KEY || "";
+        console.log(apiKey)
         const uniqueParam: string = `nocache=${Date.now()}`;
         const endpoint: string = params.endpoint;
         const url: string = `https://stackby.com/api/betav1/rowlist/stqB2IjOCulBJkhrZB/${endpoint}?${uniqueParam}`;
+        console.log(url)
+       
+        if (!apiKey) {
+            console.log("Não esta definida")
+            return NextResponse.json({
+                error: "Não esta definida",
+                status: 500
+            });
+        }
 
         const response: Response = await fetch(url, {
             method: "GET",
@@ -15,21 +26,23 @@ export async function GET(req: Request, { params }: { params: { endpoint: string
                 "Content-Type": "application/json",
             }
         });
-
+        console.log(response.status);
         if (!response.ok) {
+            console.log(response.status);
+            console.log(response.statusText);
+
             return NextResponse.json({
-                error: `Erro ao buscar dados da API: ${response.statusText}`
+                error: `Erro ao buscar dados da API: ${response.statusText}`,
             }, {
-                status: response.status
+                status: response.status,
             });
         }
 
         const data: ApiResponse = await response.json();
-        
-        return NextResponse.json(data, {
-            status: 200
-        });
+
+
     } catch (error) {
+        console.error(error);
         return NextResponse.json({
             error: `Erro interno de servidor: ${error}`
         }, {
