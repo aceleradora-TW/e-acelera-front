aa5a9a321be36ecafb38700c14145f66971ce521
Geovana Santos <geovanaj.s.profissional@gmail.com>
2024-10-25 14:30:22 -0300

    fix (src/components/PageElements/Renders/RenderDetailingThemePage/index.tsx src/types/type.ts src/service/detailingThemeService.ts) Criação depagina, renderização de rotas, Criação do service detailingThemeService- @emilycavalcanti @GeoziihDev

---
+++ src/service/detailingThemeService.ts
@@ -0,0 +1,33 @@
+import api from "@/lib/api";
+import { ApiResponse, DataItem, FilteredDetailingThemeItem } from "@/types/type";
+
+
+
+export const getThemes = async (): Promise<ApiResponse> => {
+
+    const response = await api.get<ApiResponse>("/themes"); 
+    
+ 
+    const filteredData: DataItem[] = response.data.data.map((item: any) => {
+        
+        const field: FilteredDetailingThemeItem =  {
+            title: item.field.title,
+            description: item.field.description,
+            topicsDescription: item.field.topicsDescription,
+            rowId: item.field.rowId,
+            topics:item.field.topics,
+            topicsInfo:item.field.topicsInfo
+        }
+        return { 
+            id:item.field.rowId,
+            field
+        }
+    });
+    
+    
+    
+    const data: ApiResponse = { data: filteredData };
+    console.log(data)
+    return data;
+};
+
