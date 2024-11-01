c633d4782971037e673b96c7ac317d71d7402456
Wanderson Silva <wandersonsilva@Wandersons-MacBook-Pro.local>
2024-11-01 14:06:09 -0300

    fix(src/app/api): Removendo cache de rotas da api - @Wander06 @Geoziihdev

--- src/app/api/themes/route.tsx
+++ src/app/api/themes/route.tsx
@@ -24,6 +24,7 @@ export async function GET(
                     headers: {
                         "x-api-key": apiKey,
                         "Content-Type": "application/json",
+                        "Cache-Control": "no-cache"
                     }
                 });
 
--- src/app/api/topics/route.tsx
+++ src/app/api/topics/route.tsx
@@ -21,6 +21,7 @@ export async function GET(
                     headers: {
                         "x-api-key": apiKey,
                         "Content-Type": "application/json",
+                        "Cache-Control": "no-cache"
                     }
                 });
 
