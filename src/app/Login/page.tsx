5632bbb348c9eea593c27709930ff8dae0587323
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-11-06 14:15:07 -0300

    refactor(src/app/Login/page.ts src/app/page.tsx) alteracoes na pag de login @GuusRodrigues @stephanybrazeir0

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,21 +1,28 @@
-'use client'
+"use client";
 import { CardLogin } from "@/components/CardLogin";
 import { LayoutPage } from "@/components/PageElements/LayoutPage";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Loading } from "@/components/Loading";
 
 export default function Login() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  const { data: session } = useSession()
-  const router = useRouter()
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
 
-  if(session){
-    return router.push("/")
+  if (status === "loading") {
+    return <Loading />;
   }
 
   return (
     <LayoutPage>
-      <CardLogin/>
+      <CardLogin />
     </LayoutPage>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react"
 export default function Home() {
 
   const router = useRouter()
-  router.push("/nivelamento")
+  router.push("/Nivelamento")
 
   return <Box></Box>
 }
