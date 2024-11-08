a3afd2015fa3bddf2ad66e7c6b321271c5d39cfa
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-11-08 10:39:09 -0300

    fix (src/app/api/auth/[...nextauth]/providers.ts import { getAuthProviders } from "./providers"; ) Ajustes de erros de build, @stephanybrazeir0 @GuusRodrigues

--- src/app/api/auth/[...nextauth]/providers.ts
+++ src/app/api/auth/[...nextauth]/providers.ts
@@ -14,6 +14,16 @@ const {
     LINKEDIN_CLIENT_SECRET,
 } = process.env
 
+if (
+  !GITHUB_ID ||
+  !GITHUB_SECRET ||
+  !GOOGLE_CLIENT_ID ||
+  !GOOGLE_CLIENT_SECRET ||
+  !LINKEDIN_CLIENT_ID ||
+  !LINKEDIN_CLIENT_SECRET
+) {
+  throw new Error("Missing necessary environment variables for authentication providers.");
+}
 export const getAuthProviders = () => [
     GitHubProvider({
       clientId: GITHUB_ID!,
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,6 @@
 import NextAuth from "next-auth"
 import { getAuthProviders } from "./providers";
 
-
-const {
-  GITHUB_ID,
-  GITHUB_SECRET,
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  FACEBOOK_CLIENT_ID,
-  FACEBOOK_CLIENT_SECRET,
-  LINKEDIN_CLIENT_ID,
-  LINKEDIN_CLIENT_SECRET,
-} = process.env
-
-if (
-  !GITHUB_ID ||
-  !GITHUB_SECRET ||
-  !GOOGLE_CLIENT_ID ||
-  !GOOGLE_CLIENT_SECRET ||
-  !FACEBOOK_CLIENT_ID ||
-  !FACEBOOK_CLIENT_SECRET ||
-  !LINKEDIN_CLIENT_ID ||
-  !LINKEDIN_CLIENT_SECRET
-) {
-  throw new Error("Missing necessary environment variables for authentication providers.");
-}
-
 const handler = NextAuth({
   providers: getAuthProviders(),
 });
