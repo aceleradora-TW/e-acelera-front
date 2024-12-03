fbd5d6cb479b04adfe5516255634b4ce91a127ca
Danielle Madrid <daniellereismadridd@gmail.com>
2024-12-02 21:18:19 -0300

    fix: env

--- src/app/helpers/providers.ts
+++ src/app/helpers/providers.ts
@@ -5,8 +5,8 @@ import LinkedInProvider from "next-auth/providers/linkedin"
 import logger from "@/utils/logger"
 
 const {
-  GITHUB_ID,
-  GITHUB_SECRET,
+  GIT_ID,
+  GIT_SECRET,
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   FACEBOOK_CLIENT_ID,
@@ -16,8 +16,8 @@ const {
 } = process.env
 
 if (
-  !GITHUB_ID ||
-  !GITHUB_SECRET ||
+  !GIT_ID ||
+  !GIT_SECRET ||
   !GOOGLE_CLIENT_ID ||
   !GOOGLE_CLIENT_SECRET ||
   !LINKEDIN_CLIENT_ID ||
@@ -25,20 +25,12 @@ if (
 ) {
   logger.error("Missing necessary environment variables for authentication providers.");
 
-  logger.info("GITHUB_ID: ", GITHUB_ID);
-  logger.info("GITHUB_SECRET: ", GITHUB_SECRET)
-  logger.info("GOOGLE_CLIENT_ID: ", GOOGLE_CLIENT_ID);
-  logger.info("GOOGLE_CLIENT_SECRET: ", GOOGLE_CLIENT_SECRET);
-  logger.info("LINKEDIN_CLIENT_ID: ", LINKEDIN_CLIENT_ID);
-  logger.info("LINKEDIN_CLIENT_SECRET: ", LINKEDIN_CLIENT_SECRET);
-
-
   throw new Error("Missing necessary environment variables for authentication providers.");
 }
 export const getAuthProviders = () => [
   GitHubProvider({
-    clientId: GITHUB_ID!,
-    clientSecret: GITHUB_SECRET!,
+    clientId: GIT_ID!,
+    clientSecret: GIT_SECRET!,
   }),
   GoogleProvider({
     clientId: GOOGLE_CLIENT_ID!,
