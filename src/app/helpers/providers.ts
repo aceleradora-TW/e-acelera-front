c3520e6269f1a781dbb2065453c1fcaddfcf15f4
Danielle Madrid <daniellereismadridd@gmail.com>
2024-12-02 21:08:23 -0300

    fix: log

--- src/app/helpers/providers.ts
+++ src/app/helpers/providers.ts
@@ -23,16 +23,15 @@ if (
   !LINKEDIN_CLIENT_ID ||
   !LINKEDIN_CLIENT_SECRET
 ) {
-  logger.error("Missing necessary environment variables for authentication providers.", {
-    GITHUB_ID,
-    GITHUB_SECRET,
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    FACEBOOK_CLIENT_ID,
-    FACEBOOK_CLIENT_SECRET,
-    LINKEDIN_CLIENT_ID,
-    LINKEDIN_CLIENT_SECRET,
-  });
+  logger.error("Missing necessary environment variables for authentication providers.");
+
+  logger.info("GITHUB_ID: ", GITHUB_ID);
+  logger.info("GITHUB_SECRET: ", GITHUB_SECRET)
+  logger.info("GOOGLE_CLIENT_ID: ", GOOGLE_CLIENT_ID);
+  logger.info("GOOGLE_CLIENT_SECRET: ", GOOGLE_CLIENT_SECRET);
+  logger.info("LINKEDIN_CLIENT_ID: ", LINKEDIN_CLIENT_ID);
+  logger.info("LINKEDIN_CLIENT_SECRET: ", LINKEDIN_CLIENT_SECRET);
+
 
   throw new Error("Missing necessary environment variables for authentication providers.");
 }
