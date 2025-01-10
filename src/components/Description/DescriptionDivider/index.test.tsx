fe92cb6190e4ba307af26c7764cbd2140104d0f4
Wanderson Silva <wandersonsilva@Wandersons-MacBook-Pro.local>
2025-01-10 14:46:05 -0300

    fix(src/components/Description/DescriptionDivider/index.test.tsx jest.config.ts jest.setup.js): Configurando arquivo do jest para ignorar react markdown e aceita-lo como um commonJS - @Wander06 @Stephanybrazeir0 @Geoziihdev @marvysonprpg @Lip29bm

--- src/components/Description/DescriptionDivider/index.test.tsx
+++ src/components/Description/DescriptionDivider/index.test.tsx
@@ -4,13 +4,13 @@ import {render, screen} from "@testing-library/react"
 
 
 describe("Testes nos componentes DescriptionDivider", () => {
-    it ("Verificando testes do textDivider"), () => {
+    it ("Verificando testes do textDivider", () => {
         const text = "Neste texto teremos duas partes essa que estou escrevendo é a primeira. Agora vou escrever aqui a segunda parte do texto."
         render(<DescriptionDivider text={text}/> )
-        // const firstPart = screen.getByText("Neste texto teremos duas partes essa que estou escrevendo é a primeira.")
-        // const secondPart = screen.getByText("Agora vou escrever aqui a segunda parte do texto.")
-        // expect(firstPart).toBeInTheDocument()
-        // expect(secondPart).toBeInTheDocument()
+        const firstPart = screen.getByText("Neste texto teremos duas partes essa que estou escrevendo é a primeira.")
+        const secondPart = screen.getByText("Agora vou escrever aqui a segunda parte do texto.")
+        expect(firstPart).toBeInTheDocument()
+        expect(secondPart).toBeInTheDocument()
         
-    }
+    })
 }) 
