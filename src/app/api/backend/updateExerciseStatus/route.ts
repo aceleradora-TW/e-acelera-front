7873a3ae29cf82f4bf2a102a70409e0959e8858c
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2025-01-29 15:07:24 -0300

    feat(src/app/api/backend/getExerciseStatus/route.ts src/app/api/backend/updateExerciseStatus/route.ts src/components/StatusSelect/index.tsx) criacao do put separado e refatoracao dos codigos @Geoziihdev @stephanybrazeir0

--- src/app/api/backend/updateExerciseStatus/route.ts
+++ src/app/api/backend/updateExerciseStatus/route.ts
@@ -1,11 +1,12 @@
 import { headers } from "next/headers"
 import { NextResponse } from "next/server"
 
-export async function GET(req: Request) {
+export async function PUT(req: Request) {
   const header = headers()
 
   const topicId = header.get(`topicId`)
   const itemId = header.get(`itemId`)
+  const itemStatus = header.get(`itemStatus`)
   const accessToken = header.get(`Authorization`)
 
   if (!topicId || !itemId) {
@@ -17,13 +18,17 @@ export async function GET(req: Request) {
 
   try {
     const baseUrl = process.env.BACKEND_BASE_URL
-    const response = await fetch(`${baseUrl}/topic/${topicId}/item/${itemId}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-    })
+    const response = await fetch(
+      `${baseUrl}/topic/${topicId}/item/${itemId}/status`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemStatus: itemStatus }),
+      }
+    )
 
     if (!response.ok) {
       throw new Error(
@@ -32,9 +37,8 @@ export async function GET(req: Request) {
     }
 
     const data = await response.json()
-    const statusData = data[0].itemStatus
+    return NextResponse.json({ data }, { status: 200 })
 
-    return NextResponse.json({ status: statusData }, { status: 200 })
   } catch (error) {
     console.error("Error fetching status:", error)
     return NextResponse.json(
