edd55eb89b9f5cd87cd8c232e51800d62e9aeb7a
Wanderzão <112987180+Wander06@users.noreply.github.com>
2025-02-05 10:24:05 -0300

    Revert "Refactor (src/components/StatusSelect/index.tsx) Fizemos tratativas de erro na função extractIdsFromUrl @Geoziihdev @stephanybrazeir0"
    This reverts commit db239d6e6c5cc99b2fd85cd192afe3b105dfdedc.

--- src/components/StatusSelect/index.tsx
+++ src/components/StatusSelect/index.tsx
@@ -33,7 +33,6 @@ export default function StatusSelect({ width = "30%" }: StatusSelectProps) {
 
   const extractIdsFromUrl = (pathname: string): string[] | null => {
     const parts = pathname.split("/")
-    if (parts.length === 4){
     const topicId = parts[3].split("-")[0]
     const itemId = parts[4].split("-")[0]
 
@@ -43,8 +42,6 @@ export default function StatusSelect({ width = "30%" }: StatusSelectProps) {
     }
 
     return null
-  }
-  return null
   }
 
   const fetchStatus = React.useCallback(async () => {
