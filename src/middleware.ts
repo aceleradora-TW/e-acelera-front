6143b1d9f373601d5a9ab2c790ada5aed3a15418
Wanderson Silva <wandersonsilva@Wandersons-MacBook-Pro.local>
2025-02-27 10:57:38 -0300

    feat:(src/middleware.ts) criando requisicao de login dentro do middleware - @Wander06 @geoziihdev @stephanybrazeir0

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,31 @@ export async function middleware(request: NextRequest) {
     request.cookies.get("next-auth.session-token") ||
     request.cookies.get("__Secure-next-auth.session-token")
 
-  const callbackUrl =
+    const callbackUrl =
     new URLSearchParams(request.nextUrl.search).get("callbackUrl") || "/"
 
-  const parsedUrl = new URL(callbackUrl, request.url) 
+    const parsedUrl = new URL(callbackUrl, request.url) 
 
   if (request.nextUrl.pathname.startsWith("/login") && sessionToken) {
-    return NextResponse.redirect(parsedUrl)
+    try {
+      const baseUrl = process.env.BACKEND_BASE_URL
+      const response = await fetch(`${baseUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${sessionToken.value}`,
+          "Content-Type": "application/json",
+        },
+      })
+
+      if (!response.ok) {
+        return NextResponse.json({ error: "Failed to register user" }, { status: response.status })
+      }
+
+      return NextResponse.redirect(new URL(parsedUrl, request.url))
+    } catch (error) {
+      console.error("Internal server error: ", error)
+      return NextResponse.json({ error: "Internal server error" }, { status: 500 })
+    }
   }
 
   return NextResponse.next()
