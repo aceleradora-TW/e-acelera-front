f05f4a1405ccad76f7f649ac927f502452cc953d
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2025-03-07 13:09:11 -0300

    feat ( src/components/PageElements/Renders/RenderDetailingTopicPage/index.tsx  src/components/fetchStatus/fecthStatusTopic/index.ts) adicionando req no fetchStatus e chamando no topic

--- src/components/PageElements/Renders/RenderDetailingTopicPage/index.tsx
+++ src/components/PageElements/Renders/RenderDetailingTopicPage/index.tsx
@@ -4,22 +4,37 @@ import { LayoutPage } from "../../LayoutPage";
 import { DetailingTopicContent } from "../../Content/DetailingTopicContent";
 import { BadRequest } from "@/components/BadRequest";
 import { NoData } from "@/components/NoData";
+import { useFetchTopicStatus } from "@/components/fetchStatus/fecthStatusTopic";
 
-export const RenderDetailingTopicPage = (id: string)=> {
-  const { data: renderData, isLoading: loading, error: error} = useFetchData('/api/stackbyApi/Topics');
+export const RenderDetailingTopicPage = (id: string) => {
+  const extractId = (): string  => {
+    const parts: string[] = id.split("-");
+    return parts[0] ;
+  };
+
+  const topicId = extractId();
+
+  const {
+    data: renderData,
+    isLoading: loading,
+    error: error,
+  } = useFetchData("/api/stackbyApi/Topics");
+
+  const { dataStatus } = useFetchTopicStatus(topicId);
+  console.log(dataStatus); 
   if (loading) {
-    return <Loading />
-}
-if (error) {
-    return <BadRequest />
-}
-if (!renderData) {
-    return <NoData/>
-}
-console.log(id, renderData)
+    return <Loading />;
+  }
+  if (error) {
+    return <BadRequest />;
+  }
+  if (!renderData) {
+    return <NoData />;
+  }
+  
   return (
     <LayoutPage>
-      <DetailingTopicContent data={renderData} id={id} />
+      <DetailingTopicContent data={renderData} id={id} dataStatus={dataStatus}/>
     </LayoutPage>
   );
-}
+};
--- src/components/fetchStatus/fecthStatusTopic/index.ts
+++ src/components/fetchStatus/fecthStatusTopic/index.ts
@@ -30,3 +30,38 @@
 //       console.error("Erro ao buscar status dos exercícios do tópico:", error);
 //     }
 //   }, [topicId, pageId]);
+
+import { useState, useEffect } from "react";
+
+export const useFetchTopicStatus = (topicId?: string) => {
+  const [dataStatus, setDataStatus] = useState<any>(null); 
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!topicId) return;
+
+    const fetchStatus = async () => {
+      try {
+        const response = await fetch("/api/backend/getTopicExercisesStatus", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            topicId,
+          },
+        });
+
+        if (!response.ok) throw new Error(`Erro ${response.status}`);
+
+        const data = await response.json();
+        setDataStatus(data.status); 
+      } catch (error) {
+        setError("Erro ao buscar status");
+        console.error("Erro ao buscar status dos exercícios do tópico:", error);
+      }
+    };
+
+    fetchStatus();
+  }, [topicId]);
+
+  return { dataStatus, error };
+};
