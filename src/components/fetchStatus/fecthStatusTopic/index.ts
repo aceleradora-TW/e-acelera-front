b4b4ea80175f27699189d9a589b51616b9b4206f
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2025-03-17 09:49:00 -0300

    refactor: (src/components/fetchStatus/fecthStatusTopic/index.ts) removendo o fetchStatus para fora do useEffect e apenas chamando ele @Geoziidev @Lip29bm @stephanybrazeir0

--- src/components/fetchStatus/fecthStatusTopic/index.ts
+++ src/components/fetchStatus/fecthStatusTopic/index.ts
@@ -3,31 +3,29 @@ import { useState, useEffect } from "react"
 
 export const useFetchTopicStatus = (topicId?: string) => {
   const [dataStatus, setDataStatus] = useState<ApiTopic>()
+  const fetchStatus = async () => {
+    if (!topicId) return
+    try {
+      const response = await fetch("/api/backend/getTopicExercisesStatus", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          topicId,
+        },
+      })
+
+      if (!response.ok) throw new Error(`Erro ${response.status}`)
+
+      const data = await response.json()
+      setDataStatus(data)
+    } catch (error) {
+      console.error("Erro ao buscar status dos exercícios do tópico: ", error)
+    }
+  }
 
   useEffect(() => {
-    if (!topicId) return
-
-    const fetchStatus = async () => {
-      try {
-        const response = await fetch("/api/backend/getTopicExercisesStatus", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            topicId,
-          },
-        })
-
-        if (!response.ok) throw new Error(`Erro ${response.status}`)
-
-        const data = await response.json()
-        setDataStatus(data)
-      } catch (error) {
-        console.error("Erro ao buscar status dos exercícios do tópico: ", error)
-      }
-    }
-
     fetchStatus()
-  }, [topicId])
+  })
 
   return { dataStatus }
 }
