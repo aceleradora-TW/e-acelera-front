ef53424f1c1ffc7e9f57feb6d8a3b2ee89d16474
GessicaGobbi <gessicagobbi@hotmail.com>
2025-09-09 15:27:15 -0300

    feat: implementacao dos componentes de mui @gabriellegobbi  @sanbruxo @Geoziihdev @andrea-leite

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,17 @@
 import '@testing-library/jest-dom';
-
 jest.mock('react-markdown', () => {
+    return {
+      __esModule: true,
+      default: () => <div>Mocked React Markdown</div>,
+    };
+  });
+  jest.mock('remark-gfm', () => {
+    return {
+      __esModule: true,
+      default: () => <div>Mocked React Markdown</div>,
+    };
+  });
+jest.mock('rehype-raw', () => {
     return {
       __esModule: true,
       default: () => <div>Mocked React Markdown</div>,
