f14578a30f4630f5e4b8ffe57ee01889fa71ef77
Vyni <vyni.brasil@gmail.com>
2025-09-26 11:51:46 -0300

    fix: corrigir filtro

--- src/components/PageElements/Renders/RenderDetailingThemePage/index.tsx
+++ src/components/PageElements/Renders/RenderDetailingThemePage/index.tsx
@@ -24,7 +24,7 @@ export const RenderDetailingThemePage = (id: string)=> {
   }
   return (
     <LayoutPage>
-      <DetailingThemeContent data={renderData} />
+      <DetailingThemeContent data={renderData} id={id} />
     </LayoutPage>
   );
 
