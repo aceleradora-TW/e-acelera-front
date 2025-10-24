74f2d9466481605abb404dd04b4a2ee1ed73f4ea
Jhamyllie <milli.santana2017@gmail.com>
2025-10-24 14:17:39 -0300

    fix: reorganiza seções da home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
 
   return(
     <>
-      <TestimonialsSection />
       <HomeBanner />
       <ImpactSection />
       <SponsorsSection />
+      <TestimonialsSection />
     </>
   )
 }
