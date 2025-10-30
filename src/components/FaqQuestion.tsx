fa022ff1712204831c173a4801d1c3d97ad974cb
Renchs <mathfm.dev@gmail.com>
2025-10-30 13:53:18 -0300

    chore: ajuste no espaçamento do faq

--- src/components/FaqQuestion.tsx
+++ src/components/FaqQuestion.tsx
@@ -34,27 +34,41 @@ export default function FaqQuestion() {
   ];
   
   return (
-    <Box width={"100%"} sx={{display: "flex", justifyContent: "center"}}>
-      <Grid sx={{ maxWidth: "1292px", paddingY: 4}} container spacing={{ xs:2, md:4}}>
-        <Grid item xs={12} md={4}
+    <Box
+      width={"100%"}
+      sx={{ display: "flex", justifyContent: "center", paddingY: 4 , paddingX: 2}}
+    >
+      <Grid sx={{ maxWidth: "1292px" }} container spacing={{ xs: 2, md: 4 }}>
+        <Grid
+          item
+          xs={12}
+          md={4}
           sx={{
             display: "flex",
             flexDirection: "column",
           }}
         >
-          <Typography variant="h3" gutterBottom color={theme.palette.textColor?.main}>
+          <Typography
+            variant="h3"
+            gutterBottom
+            color={theme.palette.textColor?.main}
+          >
             Tire suas dúvidas - FAQ
           </Typography>
           <Typography variant="body2">
-            Começar algo novo pode gerar muitas perguntas, e tudo bem! Aqui estão
-            as dúvidas mais comuns da nossa comunidade, respondidas de forma
-            simples e direta.
+            Começar algo novo pode gerar muitas perguntas, e tudo bem! Aqui
+            estão as dúvidas mais comuns da nossa comunidade, respondidas de
+            forma simples e direta.
           </Typography>
         </Grid>
         <Grid item xs={12} md={8}>
           {faqData.map((item, i) => (
-              <AccordionFaq question={item.question} answer={item.answer} key={i} />
-            ))}
+            <AccordionFaq
+              question={item.question}
+              answer={item.answer}
+              key={i}
+            />
+          ))}
         </Grid>
       </Grid>
     </Box>
