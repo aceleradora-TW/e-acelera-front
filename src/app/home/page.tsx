a0883370ab270ba4fb50faaff3c1118d1d04529e
Vyni <vyni.brasil@gmail.com>
2025-11-14 10:39:16 -0300

    fix: pequenas alterações no home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,15 +5,17 @@ import { TestimonialsSection } from "@/components/testimonials-section";
 import { HomeBanner } from "@/components/UI/home/home-banner";
 import { ImpactSection } from "@/components/UI/home/impact-section";
 import { SponsorsSection } from "@/components/UI/home/sponsors-section";
+import { SessionAccelerator } from "@/components/UI/home/session-the-agile-accelerator";
 
 export default function Home() {
   return (
     <>
       <HomeBanner />
+      <SessionAccelerator />
       <ImpactSection />
       <SponsorsSection />
-      <Faq />
       <TestimonialsSection />
+      <Faq />
     </>
   );
 }
