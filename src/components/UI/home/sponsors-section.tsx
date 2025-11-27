c1f128b6fc162dd051de7ec9c538c6549d1c38a1
Jhamyllie <milli.santana2017@gmail.com>
2025-11-27 09:40:31 -0300

    fix: ajustes nas logos e nas bordas das caixas de conteúdos @andrea-leite

--- src/components/UI/home/sponsors-section.tsx
+++ src/components/UI/home/sponsors-section.tsx
@@ -16,11 +16,11 @@ export const SponsorsSection = () => {
             >
             <Container maxWidth= "md"
             sx={{
-        px: {
-          xs: 3,
-          md: 0,
-        },
-      }}>
+                px: {
+                xs: 3,
+                md: 0,
+                },
+            }}>
                 <Typography
                     variant="h2"
                     sx= {{
@@ -44,132 +44,120 @@ export const SponsorsSection = () => {
             </Container>
             
             <Container>
-            <Grid container sx={{ m: 0,
-                p: 0,
-                maxWidth: "928px",
-                mx: "auto",
-             }}>
-                <Grid item xs={12} md={8}
-                sx={{
-                    mt: {xs:"16px", md: "0"},
+                <Grid container sx={{ 
+                    m: 0,
+                    p: 0,
+                    maxWidth: "928px",
+                    mx: "auto",
                 }}>
-                <Box
+                    <Grid item xs={12} md={8}
                     sx={{
-                    backgroundColor: theme.palette.textColor?.main,
-                    border: "2px solid white",
-                    borderRadius: {xs: "4px", md:"4px 0 0 4px"},
-                    overflow: "hidden",
-                    borderRight:{md:"none"},
-                    }}
-                >
-                    <Box sx={{ textAlign: "center", py: 1 }}>
-                    <Typography variant="h4">Patrocinadores</Typography>
-                    </Box>
-                </Box>
+                        mt: {xs:"16px", md: "0"},
+                    }}>
+                        <Box
+                            sx={{
+                            backgroundColor: theme.palette.textColor?.main,
+                            border: "2px solid white",
+                            borderRadius: {xs: "4px", md:"4px 0 0 4px"},
+                            overflow: "hidden",
+                            borderRight:{md:"none"},
+                            }}
+                        >
+                            <Box sx={{ textAlign: "center", py: 1 }}>
+                                <Typography variant="h4">Nossas Patrocinadoras</Typography>
+                            </Box>
+                        </Box>
 
-                <Box
+                        <Box
+                            sx={{
+                            border: "2px solid white",
+                            borderRadius: {xs: "4px", md:"4px 0 0 4px"},
+                            height: {xs: "auto", md:"115px"},
+                            overflow: "visible",
+                            borderRight:{md:"none"},
+                            }}
+                        >
+                            <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "space-around",
+                                alignItems: "center",
+                                p: 1,
+                                gap: { xs: 1, md: 2 },
+                                flexWrap: { xs: "wrap", md: "nowrap" },
+                                marginTop: "20px",
+                            }}
+                            >
+                            <Box sx={{ textAlign: "center", filter: "brightness(0) invert(1)" }}>
+                                <Image width={150} height={37} src="/assets/logoTW.webp" alt="logo TW" />
+                            </Box>
+
+                            <Box sx={{ textAlign: "center", filter: "brightness(0) invert(1)" }}>
+                                <Image width={120} height={38} src="/assets/logoGlobo.webp" alt="logo Globo" />
+                            </Box>
+
+                            <Box sx={{ textAlign: "center", filter: "brightness(0) invert(1)" }}>
+                                <Image width={80} height={28} src="/assets/logoPUC.webp" alt="logo PUC" />
+                            </Box>
+
+                            <Box sx={{ textAlign: "center", filter: "brightness(0) invert(1)" }}>
+                                <Image width={135} height={26} src="/assets/logoTecnoPUC.webp" alt="logo TecnoPUC" />
+                            </Box>
+                            </Box>
+                        </Box>
+                    </Grid>
+
+                    <Grid item xs={12} md={4}
                     sx={{
-                    border: "2px solid white",
-                    borderRadius: {xs: "4px", md:"4px 0 0 4px"},
-                    height: {xs: "auto", md:"115px"},
-                    overflow: "visible",
-                    borderRight:{md:"none"},
-                    }}
-                >
-                    <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-around",
-                        alignItems: "center",
-                        // p: 1,
-                        marginBottom: "1000px",
-                        gap: { xs: 1, md: 2 },
-                        flexWrap: { xs: "wrap", md: "nowrap" },
-                        // mt: 2
-                        marginTop: "25px",
-                        // marginTop: "20px",
-                        // marginTop: "32px"
+                        mt: {xs:"16px", md: "0"},
+                    }}>
+                        <Box
+                            sx={{
+                            backgroundColor: theme.palette.textColor?.main,
+                            border: "2px solid white",
+                            borderRadius: {xs: "4px", md:"0 4px 4px 0"},
+                            }}
+                        >
+                            <Box sx={{ textAlign: "center", py: 1 }}>
+                                <Typography variant="h4">Nossas Apoiadoras</Typography>
+                            </Box>
+                        </Box>
 
-                    }}
-                    >
-                    <Box sx={{ textAlign: "center" }}>
-                        <Image width={165} height={52} src="/assets/logoTW.webp" alt="logo TW" />
-                        {/* <Typography sx={{ 
-                            fontSize: { xs: theme.typography.body2.fontSize, md: theme.typography.body1.fontSize }, */}
-                        
-                        {/* }}>ThoughtWorks</Typography> */}
-                    </Box>
-
-                    <Box sx={{ textAlign: "center" }}>
-                        <Image width={130} height={48} src="/assets/logoGlobo.webp" alt="logo Globo" />
-                        {/* <Typography sx={{ 
-                            fontSize: { xs: theme.typography.body2.fontSize, md: theme.typography.body1.fontSize },
-                            
-                        }}>Globo</Typography> */}
-                    </Box>
-
-                    <Box sx={{ textAlign: "center" }}>
-                        <Image width={110} height={48} src="/assets/logoPUC.png" alt="logo PUC" />
-                        {/* <Typography sx={{ 
-                            fontSize: { xs: theme.typography.body2.fontSize, md: theme.typography.body1.fontSize },
-                            
-                        }}>PUCRS</Typography> */}
-                    </Box>
-
-                    <Box sx={{ textAlign: "center" }}>
-                        <Image width={155} height={36} src="/assets/logoTecnoPUC.webp" alt="logo TecnoPUC" />
-                        {/* <Typography sx={{ 
-                            fontSize: { xs: theme.typography.body2.fontSize, md: theme.typography.body1.fontSize },
-                            
-                        }}>TecnoPUC</Typography> */}
-                    </Box>
-                    </Box>
-                </Box>
+                        <Box
+                            sx={{
+                            border: "2px solid white",
+                            borderRadius: {xs: "4px", md:"0 4px 4px 0"},
+                            height: "115px",
+                            }}
+                        >
+                            <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "space-around",
+                                alignItems: "center",
+                                gap: { xs: 1, md: 2 },
+                                flexWrap: { xs: "wrap", md: "nowrap" },
+                                marginTop: "20px",
+                            }}
+                            >
+                                <Box
+                                    sx={{
+                                        filter: "brightness(0) invert(1)"
+                                    }}
+                                >
+                                    <Image width={61} height={61} src="/assets/logoDevMidia.svg" alt="logo DevMidia" />
+                                </Box>
+                                <Box
+                                    sx={{
+                                        filter: "brightness(0) invert(1)"
+                                    }}
+                                    >
+                                    <Image width={63} height={63} src="/assets/logoFeedz.svg" alt="logo Feedz" />
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Grid>
                 </Grid>
-
-                <Grid item xs={12} md={4}
-                sx={{
-                    mt: {xs:"16px", md: "0"},
-                }}>
-                <Box
-                    sx={{
-                    backgroundColor: theme.palette.textColor?.main,
-                    border: "2px solid white",
-                    borderRadius: {xs: "4px", md:"0 4px 4px 0"},
-                    }}
-                >
-                    <Box sx={{ textAlign: "center", py: 1 }}>
-                    <Typography variant="h4">Apoiadores</Typography>
-                    </Box>
-                </Box>
-
-                <Box
-                    sx={{
-                    border: "2px solid white",
-                    borderRadius: {xs: "4px", md:"0 4px 4px 0"},
-                    height: "115px",
-                    }}
-                >
-                    <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-around",
-                        alignItems: "center",
-                        p: 2,
-                        gap: { xs: 1, md: 2 },
-                        flexWrap: { xs: "wrap", md: "nowrap" },
-                    }}
-                    >
-                    <Box>
-                        <Image width={76} height={76} src="/assets/logoDevMidia.svg" alt="logo DevMidia" />
-                    </Box>
-                    <Box>
-                        <Image width={78} height={78} src="/assets/logoFeedz.svg" alt="logo Feedz" />
-                    </Box>
-                    </Box>
-                </Box>
-                </Grid>
-            </Grid>
             </Container>
 
         </Box>
