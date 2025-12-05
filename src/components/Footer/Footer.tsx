ea8978c294818199d321386d19d7f8d1abc80b72
Jhamyllie <milli.santana2017@gmail.com>
2025-12-05 09:58:33 -0300

    fix: ajuste das logos

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -19,13 +19,16 @@ export const Footer: React.FC<FooterProps> = ({ linkedinUrl, projectUrl }) => {
     ? "#222"
     : muiTheme.customStyles?.Footer?.background ??
       muiTheme.palette.primary.main;
+
   const footerText = contrastEnabled
     ? "#E0E0E0"
     : muiTheme.customStyles?.Footer?.color ??
       muiTheme.palette.primary.contrastText;
+
   const linkColor = contrastEnabled
     ? "#E0E0E0"
-    : muiTheme.palette.bgColor?.main ?? muiTheme.palette.primary.contrastText;
+    : muiTheme.palette.bgColor?.main ??
+      muiTheme.palette.primary.contrastText;
 
   return (
     <Box
@@ -38,14 +41,20 @@ export const Footer: React.FC<FooterProps> = ({ linkedinUrl, projectUrl }) => {
     >
       <Container maxWidth="xl" sx={{ pt: 4, pb: 1 }}>
         <Grid container spacing={{ xs: 4, md: 4 }}>
-
           <Grid item xs={12} md={4.5}>
-            <Typography variant="subtitle1" fontWeight="bold" sx={{ color: footerText }}>
+            <Typography
+              variant="subtitle1"
+              fontWeight="bold"
+              sx={{ color: footerText }}
+            >
               Sobre o e-Acelera
             </Typography>
 
             <Typography variant="body2" sx={{ color: footerText }}>
-              O e-Acelera é uma plataforma do Projeto Aceleradora, que impulsiona o aprendizado de desenvolvedores por meio de conteúdos práticos e colaborativos, preparando profissionais para os desafios do mercado.
+              O e-Acelera é uma plataforma do Projeto Aceleradora, que
+              impulsiona o aprendizado de desenvolvedores por meio de conteúdos
+              práticos e colaborativos, preparando profissionais para os
+              desafios do mercado.
             </Typography>
           </Grid>
 
@@ -57,14 +66,13 @@ export const Footer: React.FC<FooterProps> = ({ linkedinUrl, projectUrl }) => {
             >
               Saiba mais
             </Typography>
+
             <Link
               href={projectUrl}
               target="_blank"
               rel="noopener"
               underline="hover"
-              sx={{
-                color: linkColor,
-              }}
+              sx={{ color: linkColor }}
               variant="body2"
             >
               <Language
@@ -92,7 +100,7 @@ export const Footer: React.FC<FooterProps> = ({ linkedinUrl, projectUrl }) => {
             </Link>
           </Grid>
 
-          <Grid item xs={8} md={2} sx={{marginLeft: "2px",}}>
+          <Grid item xs={12} md={3.5}>
             <Typography
               variant="subtitle1"
               fontWeight="bold"
@@ -106,19 +114,54 @@ export const Footer: React.FC<FooterProps> = ({ linkedinUrl, projectUrl }) => {
               sx={{
                 display: "grid",
                 gridTemplateColumns: {
+                  xs: "repeat(2, 1fr)",
                   sm: "repeat(4, 1fr)",
-                  xxs: "1fr",
                 },
                 gap: 2,
-                alignItems: "left",
-                justifyItems: "left",
+                alignItems: "center",
+                justifyItems: "center",
                 filter: "brightness(0) invert(1)",
               }}
             >
-              <Image width={100} height={28} src="/assets/logoGlobo.webp" alt="logo globo"/>
-              <Image width={140} height={23} src="/assets/logoTecnoPUC.webp" alt="logo TecnoPUC"/>
-              <Image width={100} height={23} src="/assets/logoPUC.webp" alt="logo PUC" />
-              <Image width={130} height={28} src="/assets/logoTW.webp" alt="logo TW" /> 
+              <Box sx={{ maxWidth: 120, width: "100%" }}>
+                <Image
+                  src="/assets/logoGlobo.webp"
+                  alt="logo Globo"
+                  width={70}
+                  height={40}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Box>
+
+              <Box sx={{ maxWidth: 120, width: "100%" }}>
+                <Image
+                  src="/assets/logoTecnoPUC.webp"
+                  alt="logo TecnoPUC"
+                  width={70}
+                  height={80}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Box>
+
+              <Box sx={{ maxWidth: 120, width: "100%" }}>
+                <Image
+                  src="/assets/logoPUC.webp"
+                  alt="logo PUC"
+                  width={50}
+                  height={10}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Box>
+
+              <Box sx={{ maxWidth: 120, width: "100%" }}>
+                <Image
+                  src="/assets/logoTW.webp"
+                  alt="logo TW"
+                  width={100}
+                  height={60}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </Box>
             </Box>
           </Grid>
         </Grid>
