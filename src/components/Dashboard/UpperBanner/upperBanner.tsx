ce7103208954861710a996eb5a38dc058ee007a7
Pedro Idiarte Alves <pedroidiarte7@gmail.com>
2026-01-09 16:08:15 -0300

    feat: adicionado mock nos botoes do banner

--- src/components/Dashboard/UpperBanner/upperBanner.tsx
+++ src/components/Dashboard/UpperBanner/upperBanner.tsx
@@ -1,33 +1,55 @@
 import { theme } from "@/app/config/themes";
+import { BreadCrumb } from "@/components/BreadCrumb";
 import { ClickButton } from "@/components/ClickButton";
 import { Grid, Typography } from "@mui/material";
 
-export const UpperBanner = () => (
+type UpperBannerProps = {
+  title: string,
+  menuBanner?: boolean,
+  createButton?: boolean,
+  editButton?: boolean,
+  showBreadCrumb?: boolean,
+}
+
+export const UpperBanner = ({
+    title,
+    menuBanner,
+    createButton,
+    editButton,
+    showBreadCrumb,
+} : UpperBannerProps) => (
     <Grid container alignItems="center" sx={{ backgroundColor:"#E1E9EF"}} justifyContent="space-between" padding={2}>
         <Grid item>
             <Grid container>
                 <Grid item>
-                    <Typography variant="h2" >CMS e-Acelera</Typography>
+                    {showBreadCrumb && ( <BreadCrumb /> )}
+                    <Typography variant="h2" >{title}</Typography>
                 </Grid>
             </Grid>
-
-            <Grid container spacing={2} justifyContent="center">
-                <Grid item> 
-                    <ClickButton title="Themes" click={() => console.log("Themes")}/>   
-                </Grid>   
-                <Grid item> 
-                    <ClickButton title="Topics" click={() => console.log("Topics")}/>   
-                </Grid>   
-                <Grid item> 
-                    <ClickButton title="Exercises" click={() => console.log("Exercises")}/>   
-                </Grid>   
-                <Grid item> 
-                    <ClickButton title="Videos" click={() => console.log("Videos")}/>   
-                </Grid>   
-            </Grid>
+            { menuBanner &&(
+                <Grid container spacing={2} justifyContent="center">
+                    <Grid item> 
+                        <ClickButton title="Themes" click={() => console.log("Themes")}/>   
+                    </Grid>   
+                    <Grid item> 
+                        <ClickButton title="Topics" click={() => console.log("Topics")}/>   
+                    </Grid>   
+                    <Grid item> 
+                        <ClickButton title="Exercises" click={() => console.log("Exercises")}/>   
+                    </Grid>   
+                    <Grid item> 
+                        <ClickButton title="Videos" click={() => console.log("Videos")}/>   
+                    </Grid>   
+                </Grid>
+            )}
         </Grid>
         <Grid item>
-             <ClickButton title="Criar" click={() => console.log("Criar")}/>   
+            { createButton && (
+                 <ClickButton title="Criar" click={() => console.log("Criar")}/>   
+            )}
+            { editButton && (
+                 <ClickButton title="Editar" click={() => console.log("Editar")}/>   
+            )}
         </Grid>
     </Grid>
   )
