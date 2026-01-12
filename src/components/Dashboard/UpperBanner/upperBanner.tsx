758948f394a54c5647573d86b7f024daba2e2775
Franciele Tadiello <franciele.p.t@gmail.com>
2026-01-12 13:41:30 -0300

    feat: adicionar estilos e lógica do botão criar

--- src/components/Dashboard/UpperBanner/upperBanner.tsx
+++ src/components/Dashboard/UpperBanner/upperBanner.tsx
@@ -1,14 +1,16 @@
-import { theme } from "@/app/config/themes";
 import { BreadCrumb } from "@/components/BreadCrumb";
 import { ClickButton } from "@/components/ClickButton";
 import { Grid, Typography } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import { useState } from "react";
+import { customStyles } from "@/app/config/themes/components";
 
 type UpperBannerProps = {
-  title: string,
-  menuBanner?: boolean,
-  createButton?: boolean,
-  editButton?: boolean,
-  showBreadCrumb?: boolean,
+    title: string,
+    menuBanner?: boolean,
+    createButton?: boolean,
+    editButton?: boolean,
+    showBreadCrumb?: boolean,
 }
 
 export const UpperBanner = ({
@@ -17,40 +19,55 @@ export const UpperBanner = ({
     createButton,
     editButton,
     showBreadCrumb,
-} : UpperBannerProps) => (
-    <Grid container alignItems="center" sx={{ backgroundColor:"#E1E9EF"}} justifyContent="space-between" padding={2}>
+}: UpperBannerProps) => {
+    const [selectedTab, setSelectedTab] = useState<"themes" | "topics" | "exercises" | "videos">("themes"); 
+    const handleCreate = () => { 
+        switch (selectedTab) { 
+            case "themes": console.log("Abrir criação de Themes"); 
+            break; 
+            case "topics": console.log("Abrir criação de Topics"); 
+            break; 
+            case "exercises": console.log("Abrir criação de Exercises"); 
+            break; 
+            case "videos": console.log("Abrir criação de Videos"); 
+            break; 
+            default: break; } };
+    return (
+    
+    <Grid container spacing={2} alignItems="center" justifyContent="space-between" sx={customStyles.upperBanner.container} >
         <Grid item>
             <Grid container>
                 <Grid item>
-                    {showBreadCrumb && ( <BreadCrumb /> )}
-                    <Typography variant="h2" >{title}</Typography>
+                    {showBreadCrumb && (<BreadCrumb />)}
+                    <Typography variant="h2" sx={customStyles.upperBanner.title} >{title}</Typography>
                 </Grid>
             </Grid>
-            { menuBanner &&(
+            {menuBanner && (
                 <Grid container spacing={2} justifyContent="center">
-                    <Grid item> 
-                        <ClickButton title="Themes" click={() => console.log("Themes")}/>   
-                    </Grid>   
-                    <Grid item> 
-                        <ClickButton title="Topics" click={() => console.log("Topics")}/>   
-                    </Grid>   
-                    <Grid item> 
-                        <ClickButton title="Exercises" click={() => console.log("Exercises")}/>   
-                    </Grid>   
-                    <Grid item> 
-                        <ClickButton title="Videos" click={() => console.log("Videos")}/>   
-                    </Grid>   
+                    <Grid item>
+                        <ClickButton title="Themes" click={() => console.log("Themes")} />
+                    </Grid>
+                    <Grid item>
+                        <ClickButton title="Topics" click={() => console.log("Topics")} />
+                    </Grid>
+                    <Grid item>
+                        <ClickButton title="Exercises" click={() => console.log("Exercises")} />
+                    </Grid>
+                    <Grid item>
+                        <ClickButton title="Videos" click={() => console.log("Videos")} />
+                    </Grid>
                 </Grid>
             )}
         </Grid>
         <Grid item>
-            { createButton && (
-                 <ClickButton title="Criar" click={() => console.log("Criar")}/>   
+            {createButton && (
+                <ClickButton title="Criar" click={handleCreate} sx={ customStyles.upperBanner.createButton } />
             )}
-            { editButton && (
-                 <ClickButton title="Editar" click={() => console.log("Editar")}/>   
+            {editButton && (
+                <ClickButton title="Editar" backIcon={<EditIcon />} click={() => console.log("Editar")} sx={customStyles.upperBanner.editButton} />
             )}
         </Grid>
     </Grid>
-  )
-   
+    );
+}
+
