4959eabb30471010669018af25db787c1efea4d6
Franciele Tadiello <franciele.p.t@gmail.com>
2026-01-16 09:49:00 -0300

    fix: remover upper-banner da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { ImpactSection } from "@/components/UI/home/impact-section";
 import { SponsorsSection } from "@/components/UI/home/sponsors-section";
 import AboutAgil from "@/components/UI/home/about-agile";
 import { StudySection } from "@/components/UI/home/study-section";
-import { UpperBanner } from "@/components/UI/dashboard/upper-banner";
 
 export default function Home() {
   return (
@@ -22,8 +21,6 @@ export default function Home() {
       }}
     >
       <HomeBanner />
-      <UpperBanner title="titulo" menuBanner createButton editButton showBreadCrumb />
- 
       <AboutAgil />
       <ImpactSection />
       <SponsorsSection />
