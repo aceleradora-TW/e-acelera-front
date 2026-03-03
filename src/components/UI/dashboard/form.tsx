f3b22f636d9c83d999d5f38e6947636c3022ab09
Jamile <milli.santana2017@gmail.com>
2026-03-03 13:38:54 -0300

    fix: instala dependencias

--- src/components/UI/dashboard/form.tsx
+++ src/components/UI/dashboard/form.tsx
@@ -65,7 +65,6 @@ export default function Form() {
                       fullWidth
                     />
                   )
-
                 default:
                   return (
                     <TextField
