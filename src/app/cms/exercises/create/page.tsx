000f69171b244daa09a4d04174315a83dd820460
Jamile <milli.santana2017@gmail.com>
2026-05-22 16:07:47 -0300

    fix: criacao da pasta create e arquivo page.tsx para exercicios resf #298

---
+++ src/app/cms/exercises/create/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+// import { ExerciseFormData } from "@/components/UI/dashboard/forms/defs/exercise.defs";
+import Form from "@/components/UI/dashboard/form";
+// import { createExercise } from "@/utils/api/exercises";
+import { useRouter } from "next/navigation";
+
+export default function CreateExercisePage() {
+  const router = useRouter();
+
+//   const handleSubmit = async (data: ExerciseFormData) => {
+//     await createExercise(data);
+//     router.push("/cms/exercises");
+//   };
+
+//   return <Form title="Criar Novo Exercício" onSubmit={} />;
+}   
